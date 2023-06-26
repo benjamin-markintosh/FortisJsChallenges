d0256f78afb34674d617e523ffececa5e6e7b010
marvo-gift <105926625+marvo-gift@users.noreply.github.com>
2023-06-26 22:09:27 +0400

    copy same function

--- Set2/Question5.js
+++ Set2/Question5.js
@@ -11,3 +11,21 @@ function singleElement(nums) {
   
     return set.values().next().value;
   }
+
+  
+
+  function singleElement(nums) {
+    const set = new Set();
+  
+    for (num of nums) {
+      if (set.has(num)) {
+        set.delete(num);
+      } else {
+        set.add(num);
+      }
+    }
+  
+    return set.values().next().value;
+  }
+
+  
