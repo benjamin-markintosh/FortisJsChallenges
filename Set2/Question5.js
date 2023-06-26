a13d827f331368b7e6bf83ceb4661f544d17b5aa
Wanjala Benjamin Emmanuel <121640490+Benjamin-0012@users.noreply.github.com>
2023-06-26 20:26:38 +0300

    Question 5 Wanjala

---
+++ Set2/Question5.js
@@ -0,0 +1,13 @@
+function singleElement(nums) {
+    const set = new Set();
+  
+    for (num of nums) {
+      if (set.has(num)) {
+        set.delete(num);
+      } else {
+        set.add(num);
+      }
+    }
+  
+    return set.values().next().value;
+  }
